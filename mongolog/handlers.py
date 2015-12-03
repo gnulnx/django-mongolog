53b226e24e64bc3329696a0d45e9286b36481f61
John Furr <jfurr@hbs.edu>
2015-12-03 18:27:52 -0500

    more 2to3

--- mongolog/handlers.py
+++ mongolog/handlers.py
@@ -16,6 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+from __future__ import print_function
 from logging import Handler, StreamHandler, NOTSET
 from datetime import datetime
 
