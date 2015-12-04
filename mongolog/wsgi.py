4087cb5b808d9a7f171dc679efbf922fe49fdc14
John Furr <jfurr@Johns-MacBook-Pro.local>
2015-12-04 16:09:28 -0500

    commnented out unused code paths

--- mongolog/wsgi.py
+++ mongolog/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
+"""
+# uncomment when needed
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mongolog.settings")
 
 application = get_wsgi_application()
+"""
