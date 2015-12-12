305e018171c9298188996ef0c114ac3b802aec80
John Furr <jfurr@Johns-MacBook-Pro.local>
2015-12-11 19:23:42 -0500

    Another python 3 test

--- mongolog/tests.py
+++ mongolog/tests.py
@@ -241,7 +241,8 @@ class TestSimpleMongoLogHandler(unittest.TestCase, TestRemoveEntriesMixin):
             set(['_id', 'exception', 'name', 'thread', 'time', 'process', 'level', 'msg', 'path', 'module', 'line', 'func', 'filename'])
         )
 
-        logger.warn(log_msg)
+        logger.info("Just some friendly info")
+        #logger.warn(log_msg)
         #log_msg = {'test': True, 'fruits': ['apple', 'orange'], 'error': str(ValueError), 'handler': str(SimpleMongoLogHandler())}
         #try:
         #    raise ValueError
