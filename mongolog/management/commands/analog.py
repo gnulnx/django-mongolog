0b3d01c25d7845ece15b82110b4d9db0b2dce8dc
John Furr <jfurr@hbs.edu>
2016-09-20 08:41:35 -0400

    using opt parse on older django

--- mongolog/management/commands/analog.py
+++ mongolog/management/commands/analog.py
@@ -2,6 +2,7 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import logging
+import django
 
 import pymongo
 pymongo_version = int(pymongo.version.split(".")[0])
@@ -15,12 +16,23 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+   if django.VERSION[1] <= 7:
+        from optparse import make_option
+        option_list = BaseCommand.option_list + (
+        make_option('--delete',
+            action='store_true',
+            dest='delete',
+            default=False,
+            help='Delete poll instead of closing it'),
+        )
+
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            '-l', '--limit', default=10, type=int, action='store', dest='limit',
-            help='Limit Results',
-        )
+        if django.VERSION[1] >= 7:
+            parser.add_argument(
+                '-l', '--limit', default=10, type=int, action='store', dest='limit',
+                help='Limit Results',
+            )
 
     def handle(self, *args, **options):
         handler = get_mongolog_handler()
