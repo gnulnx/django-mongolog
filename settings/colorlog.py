f04aa81b78665b48f1ae38f7505c42d53612f766
John Furr <jfurr@Johns-MacBook-Pro.local>
2016-09-24 12:23:42 -0400

    Added color logging support

--- settings/colorlog.py
+++ settings/colorlog.py
@@ -1,12 +1,14 @@
-from logutils.colorize import ColorizingStreamHandler
+import logging
+import logging.config
 
+from logutils.colorize import ColorizingStreamHandler
 
 class ColorLogHandler(ColorizingStreamHandler):
     def __init__(self, *args, **kwargs):
         super(ColorLogHandler, self).__init__(*args, **kwargs)
         self.level_map = {
             # Provide you custom coloring information here
-            logging.DEBUG: (None, 'cyan3', False),
+            logging.DEBUG: (None, 'cyan', False),
             logging.INFO: (None, 'green', False),
             logging.WARNING: (None, 'yellow', False),
             logging.ERROR: (None, 'red', False),
