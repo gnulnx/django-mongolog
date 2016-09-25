46d90edd5a0d6f72718f44fda51e5cfcff39eda4
John Furr <jfurr@Johns-MacBook-Pro.local>
2016-09-25 10:55:46 -0400

    Added TEST_VERBOSITY

--- settings/settings.py
+++ settings/settings.py
@@ -16,6 +16,7 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+TEST_VERBOSITY = False
 LOGGING = {
     'version': 1,
     'handlers': {
@@ -55,7 +56,7 @@ LOGGING = {
 
             # utc/local.  Only used with record_type=simple
             'time_zone': 'local',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'record_type': 'reference',
         },
         'test_embedded': {
@@ -67,7 +68,7 @@ LOGGING = {
 
             # utc/local.  Only used with record_type=simple
             'time_zone': 'local',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'record_type': 'embedded',
         },
         'test_base_reference': {
@@ -79,7 +80,7 @@ LOGGING = {
 
             # utc/local.  Only used with record_type=simple
             'time_zone': 'local',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'record_type': 'reference',
         },
         'test_base_reference_w0': {
@@ -91,7 +92,7 @@ LOGGING = {
 
             # utc/local.  Only used with record_type=simple
             'time_zone': 'local',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'record_type': 'reference',
         },
         'test_base_invalid': {
@@ -103,7 +104,7 @@ LOGGING = {
 
             # utc/local.  Only used with record_type=simple
             'time_zone': 'local',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'record_type': 'reference',
         },
         'test_verbose': {
@@ -115,7 +116,7 @@ LOGGING = {
 
             # utc/local.  Only used with record_type=simple
             'time_zone': 'local',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'record_type': 'reference',
         },
         'test_console': {
@@ -131,7 +132,7 @@ LOGGING = {
             # Interesting Note:  requests 2.8.1 will turn this into a GET if it's missing a trailing slash
             # We automagically add the trailing slash
             'client_auth': 'http://192.168.33.51/4e487f07a84011e5a3403c15c2bcc424',
-            'verbose': True,
+            'verbose': TEST_VERBOSITY,
             'timeout': 0.001,
         },
     },
