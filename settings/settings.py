477ae2947fcaee557d87d554626d646d4621c996
John Furr <jfurr@Johns-MacBook-Pro.local>
2016-09-28 19:09:34 -0400

    updated max_keep to 10 to see if tests behave better on travis...

--- settings/settings.py
+++ settings/settings.py
@@ -70,7 +70,7 @@ LOGGING = {
             'time_zone': 'local',
             'verbose': TEST_VERBOSITY,
             'record_type': 'embedded',
-            'max_keep': 6,
+            'max_keep': 10,
         },
         'test_base_reference': {
             'level': 'DEBUG',
