80d83dbe6c05907f9a907a7d31e0a71969603b01
Furr <jfurr@ip-10-0-1-13.ec2.internal>
2018-09-25 09:09:57 -0400

    .

--- mongolog/management/commands/mongolog_purge.py
+++ mongolog/management/commands/mongolog_purge.py
@@ -64,7 +64,7 @@ class Command(BaseCommand):
             ans = 'n'
             while 1:
                 console.warn("Would you like to proceed?  Y/N")
-                ans = input()
+                ans = input().strip()
                 if ans.lower() not in ['y', 'yes', 'n', 'no']:
                     continue
                 elif ans[0] == 'n':
